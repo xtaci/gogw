659e595c9280ec777a1a755276ce437554798201
xtaci <daniel820313@gmail.com>
2021-04-16 11:40:18 +0800

    upd gaio deps

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/libp2p/go-reuseport v0.0.2
 	github.com/valyala/bytebufferpool v1.0.0
-	github.com/xtaci/gaio v1.2.10-0.20210405132439-781304e11657
+	github.com/xtaci/gaio v1.2.10-0.20210416033415-210ff687daf8
 )
--- serve.go
+++ serve.go
@@ -4,43 +4,61 @@ import (
 	"log"
 	"net"
 
+	reuse "github.com/libp2p/go-reuseport"
 	"github.com/xtaci/gaio"
 )
 
+type Server struct {
+	// addr optionally specifies the TCP address for the server to listen on,
+	// in the form "host:port". If empty, ":http" (port 80) is used.
+	// The service names are defined in RFC 6335 and assigned by IANA.
+	// See net.Dial for details of the address format.
+	addr string
+	proc *AIOHttpProcessor // I/O processor
+}
+
 func ListenAndServe(addr string, numServer int, bufSize int) error {
-	if addr == "" {
-		addr = ":http"
-	}
-
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
-		return err
-	}
-
-	var processors []*AIOHttpProcessor
 	for i := 0; i < numServer; i++ {
 		watcher, err := gaio.NewWatcherSize(bufSize)
 		if err != nil {
 			return err
 		}
 		proc := NewAIOHttpProcessor(watcher)
-		proc.StartProcessor()
-		processors = append(processors, proc)
+		server := &Server{addr: addr, proc: proc}
+		go server.ListenAndServe()
 		log.Println(i)
+	}
+	select {}
+	return nil
+}
+
+func (srv *Server) ListenAndServe() error {
+	addr := srv.addr
+	if addr == "" {
+		addr = ":http"
+	}
+
+	ln, err := reuse.Listen("tcp", addr)
+	if err != nil {
+		return err
 	}
 
 	// start processor loop
-	r := 0
+	srv.proc.StartProcessor()
+
+	return srv.Serve(ln)
+}
+
+func (srv *Server) Serve(ln net.Listener) error {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			return err
 		}
 
-		err = processors[r].AddConn(conn)
+		err = srv.proc.AddConn(conn)
 		if err != nil {
 			return err
 		}
-		r = (r + 1) % numServer
 	}
 }
