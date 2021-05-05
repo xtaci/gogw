5e875398fa711eb6ed9fd92c66978021c93e86bd
xtaci <daniel820313@gmail.com>
2021-05-05 14:07:33 +0800

    remove preforked server

--- tests/server/main.go
+++ tests/server/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
-	"os"
-	"os/exec"
-	"strconv"
 
 	"github.com/xtaci/aiohttp"
 )
@@ -23,39 +19,15 @@ func handler(ctx *aiohttp.AIOHttpContext) {
 	}
 }
 
-const (
-	WORKER       = "AIOHTTP-WORKER"
-	WORKER_IDENT = "AIOHTTP-WORKER-WORKER"
-)
-
 func main() {
 	const numServer = 4
-	worker := os.Getenv(WORKER)
-	if worker == "" {
-		go func() {
-			log.Println(http.ListenAndServe(":6060", nil))
-		}()
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 
-		var cmds []*exec.Cmd
-		os.Setenv(WORKER, "1")
-		for i := 0; i < numServer; i++ {
-			os.Setenv(WORKER_IDENT, fmt.Sprint(i))
-			cmd := exec.Command(os.Args[0])
-			cmd.Stdin = os.Stdin
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Start()
-			cmds = append(cmds, cmd)
-		}
-
-		for k := range cmds {
-			cmds[k].Wait()
-		}
-	} else if worker == "1" {
-		workernumber, err := strconv.Atoi(os.Getenv(WORKER_IDENT))
-		if err != nil {
-			panic(err)
-		}
-		aiohttp.ListenAndServe(":8080", workernumber, 256*1024*1024, handler)
+	for i := 0; i < numServer; i++ {
+		go aiohttp.ListenAndServe(":8080", i, 256*1024*1024, handler)
 	}
+
+	select {}
 }
